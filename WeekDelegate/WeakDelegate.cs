67807299ed881ee925b022b2ae4045517ab866aa
mbv <terehovk@ya.ru>
2016-09-13 22:54:23 +0300

    Create WeakDelegate class. Add some tests without implementation

---
+++ WeekDelegate/WeakDelegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WeekDelegate
+{
+    public class WeakDelegate
+    {
+
+        private Delegate _proxyDelegate;
+
+        public Delegate Week => _proxyDelegate;
+
+        public WeakDelegate(Delegate eventHandler)
+        {
+            _proxyDelegate = eventHandler;
+        }
+    }
+}
---
+++ WeekDelegate_UnitTests/WeakDelegateTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeekDelegate_UnitTests
+{
+    [TestClass]
+    public class WeakDelegateTests
+    {
+        [TestMethod]
+        public void TestMemoryLeak()
+        {
+        }
+
+        [TestMethod]
+        public void TestDeadWeekReferance()
+        {
+        }
+    }
+}
